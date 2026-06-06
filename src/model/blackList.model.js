6f403188b3d1e205ab44d9d41896edb6b355dc61
Anjali <113291155+Anjali4402@users.noreply.github.com>
2026-06-06 22:24:36 +0530

    created model for token-blacklist

---
+++ src/model/blackList.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const tokenBlackListSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: [true, "token is required to blacklist"],
+      unique: [true, "Token is already blacklisted"],
+    },
+    blacklistedAt: {
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+tokenBlackListSchema.index(
+  { blacklistedAt: 1 },
+  {
+    expireAfterSeconds: 60 * 60 * 24 * 3, // 3 days
+  },
+);
+
+const tokenBlackListModel = mongoose.model(
+  "tokenBlackList",
+  tokenBlackListSchema,
+);
+module.exports = tokenBlackListModel;
